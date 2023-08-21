00828e50bf03c06c04aed4301c3c9fea615c592b
Malav2364 <95128635+Malav2364@users.noreply.github.com>
2023-08-21 09:45:20 +0000

    completed

---
+++ Basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treats the js code as a newer version of javascript
+
+// alert('hello'); //we are using node js , not browser
+
+//avoid semicolons in node js and react
+
+// console.log(30+6)        //code redablit should be high so write code pretty ;-)
+
+// console.log("Malav Patel")
+
+let Uname = 'Malav Patel' //data type is string 
+let age = 19 //data type is number
+let State = null // a standalone value is 
+let isLoggedIn = false //boolean data type 
+
+/*
+    data types
+    number
+    string
+    boolean
+    null is a object
+    undefined
+    symbol used for uniqueness of the components 
+*/
+
+// console.table([Uname, age, isLoggedIn, State])
+console.log(typeof 112.5) //used to determine the datatype
