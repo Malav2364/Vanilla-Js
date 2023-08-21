89f95768e450c9c1aa335ab8e8ffa11c5f1f07cd
Malav2364 <95128635+Malav2364@users.noreply.github.com>
2023-08-21 17:08:29 +0000

    conversion of DataTypes

---
+++ Basics/datatypeconv.js
@@ -0,0 +1,36 @@
+let score  =  'Malav'
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// if we convert a '33' (number in a string) it will convert in number like 33
+// if we convert '33abc' then it will convert in number but will return NaN(Not a Number)
+/*
+    "33" => 33
+    "33abc" => NaN (not a number)
+    true  => 1
+    false => 0
+*/
+
+let isLoggedIn = 'Malav'
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//conversion in boolean 
+
+/*
+    1 => true
+    0 => false
+    "Malav" => true
+*/
+
+let aNumber = 33
+
+let Snumber = String(aNumber)
+console.log(Snumber);
+console.log(typeof Snumber);
