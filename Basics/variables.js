7b276b22d69b859410449a319d070e61f9a79073
Malav2364 <95128635+Malav2364@users.noreply.github.com>
2023-08-21 09:24:08 +0000

    done

---
+++ Basics/variables.js
@@ -0,0 +1,20 @@
+const accountId = 123456  //cannot change the value :-)
+
+let accountEmail = 'malav@google.com' //can be changed with other values
+var accountPassword = '123456' /*Prefer not to use var due to block issue*/
+
+let accountState; //if no value then it is undefiened
+
+accountCity = 'Jaipur' //it uses variable memory i.e, it is a variable
+
+// accountId = 2   //cannot change the value of the const variable or not allowed
+
+accountEmail = 'abc@google.com'
+
+accountPassword = '1212121221'
+
+accountCity = 'Bengaluru'
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
