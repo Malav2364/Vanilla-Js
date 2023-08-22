26a642319719d2620572ea36d3f1782c53af0173
Malav2364 <95128635+Malav2364@users.noreply.github.com>
2023-08-22 14:10:02 +0000

    updated

---
+++ Basics/dttypesumm.js
@@ -0,0 +1,36 @@
+//  Primitive Data Types
+
+// 7 Types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+
+let score;
+console.log(typeof score);
+
+const id = Symbol('123')
+const anotherId = Symbol('121')
+
+console.log(id === anotherId);
+
+// const bigNumber = 123456789146456n          //use n to make a number a big int
+console.log(typeof bigNumber);
+
+const heros = ['shaktiman', 'naagraj', 'doga']
+
+let Obj = {
+    uName:'Malav',
+    lName:'Patel',
+    age:19,
+}
+
+const myFunction = function(){
+    console.log('Hello World!!');
+}
+
+console.log(typeof );
+
+
+//Reference Types or Non Primitive (direct reference in the memory)
+
+// Types :  Arrays, Objects, Functions 
+
