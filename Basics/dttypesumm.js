88f53e0ab6a1ff882300d8342103a81fa8f6c882
Malav2364 <95128635+Malav2364@users.noreply.github.com>
2023-08-23 04:59:45 +0000

    updated!!

--- Basics/dttypesumm.js
+++ Basics/dttypesumm.js
@@ -34,3 +34,28 @@ console.log(typeof myFunction);
 
 // Types :  Arrays, Objects, Functions 
 
+
+// Types of memory Stack (Primitive) and Heap (Non-Primitive)
+
+let myName = "My Name is Malav Patel"
+                                                                                        // its creates a copy and assigns it to the other variable
+let anotherName = myName
+anotherName = 'Coding is bekar !!!!!!!!'
+
+console.log(anotherName);
+console.log(myName);
+
+let user = {
+    email : 'abc@gmail.com',      //heap
+    upi : 'user@sbi'
+}
+
+let user2 = user      //stack
+                                        // here in this case we are giving refrence of user1 init it is not creating any copy of the user1 
+console.log(user2);
+
+user2.email = 'ms@google.com'
+
+console.log(user2.email);                       //here it gives the same refernce to both the parameters in the objects 
+console.log(user.email);
+
