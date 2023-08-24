e822b46521899c7cd96cf52a5035d54c961fe2a1
Malav2364 <95128635+Malav2364@users.noreply.github.com>
2023-08-24 11:21:27 +0000

    objects!!

---
+++ Basics2/objects.js
@@ -0,0 +1,36 @@
+// objects (singleton)
+
+// objects literals
+
+
+const mySym= Symbol("key1")
+
+const  mName = {
+    "aName" : "Malav",
+    sName: "Patel",
+    [mySym]: "myKey1",     //keys are called using square bracket
+    age: 19,
+    location: "Vadodara",
+    isloggedIn: false,
+    lastLoginDays: ["Monday", "Tuesday", "Sunday"]
+}
+// Object.freeze(mName) // important to mark as freeze so that no one can change the value of the object
+
+// console.log(mName.aName); //bad practise 
+console.log(typeof(mName[mySym]));//good practice
+
+mName.aName = "MALAV" //we can overwrite the values
+console.log(mName["aName"]);
+
+console.log(mName);
+
+mName.greeting = function(){
+    console.log("Howdy!");
+}
+
+
+mName.greeting2 = function(){
+    console.log(`Howdy! ${this.aName}`); //this is called string interpolation usingbackticks
+}
+
+console.log(mName.greeting2());
