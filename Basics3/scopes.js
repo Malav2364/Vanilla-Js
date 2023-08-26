537b8c6e6723d4fc7a792e301b94cf179804cec3
Malav2364 <95128635+Malav2364@users.noreply.github.com>
2023-08-26 06:15:42 +0000

    scopes!!

---
+++ Basics3/scopes.js
@@ -0,0 +1,14 @@
+let a  = 300  //ye achi baat kahi aap ne
+if (true) {
+    let a = 10     //block scope
+    const  b = 20    //scopes
+    console.log("INNER: "  ,  a)
+}
+
+//global scope
+
+
+
+console.log(a)
+// console.log(b)
+// console.log(c)
