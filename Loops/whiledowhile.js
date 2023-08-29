33277d21fc889ddde8402b52c9648f7a6ead0d8a
Malav2364 <95128635+Malav2364@users.noreply.github.com>
2023-08-29 13:06:32 +0000

    while do while loops!!

---
+++ Loops/whiledowhile.js
@@ -0,0 +1,23 @@
+
+let index = 0
+while (index<=10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['Shakti', 'Bhakti']
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray}`);
+    arr++
+}
+
+
+//do while loop
+
+let score = 11
+
+do{
+    console.log(`The Score is ${score}`);
+    score++                                              //udhari loop
+}while (score <= 10) 
